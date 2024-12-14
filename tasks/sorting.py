24e509fc23f8dc8d2fa1c299b1afde12c381db38
Awni <awnyaltabaa@gmail.com>
2024-12-13 19:19:24 -0500

    added BOS and EOS option

--- tasks/sorting.py
+++ tasks/sorting.py
@@ -27,7 +27,9 @@ def generate_batch(max_value, sequence_length, batch_size):
     return x, y
 
 class SortingDataset(Dataset):
-    def __init__(self, max_value, sequence_length, batch_size, num_samples, random_sequence_length=False, min_sequence_length=None, device=None):
+    def __init__(self,
+        max_value, sequence_length, batch_size, num_samples,
+        random_sequence_length=False, min_sequence_length=None, include_bos_eos=False, device=None):
 
         self.max_value = max_value
         self.sequence_length = sequence_length
@@ -36,6 +38,10 @@ class SortingDataset(Dataset):
         self.num_samples = num_samples
         self.batch_size = batch_size
         self.device = device
+        self.include_bos_eos = include_bos_eos
+        if self.include_bos_eos:
+            self.bos = max_value
+            self.eos = max_value + 1
 
     def __len__(self):
         return self.num_samples
@@ -48,6 +54,10 @@ class SortingDataset(Dataset):
             L = self.sequence_length
 
         x, y = generate_batch(max_value=self.max_value, sequence_length=L, batch_size=self.batch_size)
+        if self.include_bos_eos:
+            x = torch.cat([torch.full((self.batch_size, 1), self.bos), x, torch.full((self.batch_size, 1), self.eos)], dim=1)
+            y = torch.cat([torch.full((self.batch_size, 1), self.bos), y, torch.full((self.batch_size, 1), self.eos)], dim=1)
+
         if self.device is not None:
             x, y = x.to(self.device), y.to(self.device)
         return x, y
