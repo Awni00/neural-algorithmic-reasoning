75ed83dc0fe3e436bee1d14f3175e6d6e788d13c
Awni <awnyaltabaa@gmail.com>
2024-12-28 15:31:08 -0500

    added cosine+warmup learning rate schedule

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,11 @@
+import  math
+from typing import Any, Dict
+
 import torch
 from torch import nn
 import torch.nn.functional as F
-from typing import Any, Dict
-import torch
+from torch.optim.lr_scheduler import LambdaLR
+
 
 # region: AttributeDict
 
@@ -55,6 +58,9 @@ class AttributeDict(Dict):
 
 # endregion
 
+
+# region: general helpers and utilities
+
 def print_gpu_info():
     if torch.cuda.is_available():
         num_gpus = torch.cuda.device_count()
@@ -66,7 +72,15 @@ def print_gpu_info():
     else:
         print("No GPU available, using CPU")
 
-# region: general helpers and utilities
+def format_large_number(number):
+    if number < 1e3:
+        return str(number)
+    elif number < 1e6:
+        return f'{number/1e3:.1f}K'
+    elif number < 1e9:
+        return f'{number/1e6:.1f}M'
+    else:
+        return f'{number/1e9:.1f}B'
 
 def exists(val):
     return val is not None
@@ -104,3 +118,51 @@ def Sequential(*modules):
     return nn.Sequential(*filter(exists, modules))
 
 # endregion
+
+# region Training utilities, e.g., learning rate scheduling, etc.
+
+def get_cosine_schedule_with_warmup(
+    optimizer: torch.optim.Optimizer,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    num_cycles: float = 0.5,
+    last_epoch: int = -1,
+) -> LambdaLR:
+    """
+    Create a learning rate schedule that linearly increases the learning rate from
+    0.0 to lr over ``num_warmup_steps``, then decreases to 0.0 on a cosine schedule over
+    the remaining ``num_training_steps-num_warmup_steps`` (assuming ``num_cycles`` = 0.5).
+
+    Source: https://github.com/pytorch/torchtune/blob/32e265d5749fd592711a03247486eafa6c898d94/torchtune/training/lr_schedulers.py#L15
+    This is based on the Hugging Face implementation
+    https://github.com/huggingface/transformers/blob/v4.23.1/src/transformers/optimization.py#L104.
+
+    Args:
+        optimizer (torch.optim.Optimizer): The optimizer for which to
+            schedule the learning rate.
+        num_warmup_steps (int): The number of steps for the warmup phase.
+        num_training_steps (int): The total number of training steps.
+        num_cycles (float): The number of waves in the cosine schedule. Defaults to 0.5
+            (decrease from the max value to 0 following a half-cosine).
+        last_epoch (int): The index of the last epoch when resuming training. Defaults to -1
+
+    Returns:
+        torch.optim.lr_scheduler.LambdaLR with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step: int) -> float:
+        # linear warmup phase
+        if current_step < num_warmup_steps:
+            return current_step / max(1, num_warmup_steps)
+
+        # cosine
+        progress = (current_step - num_warmup_steps) / max(
+            1, num_training_steps - num_warmup_steps
+        )
+
+        cosine_lr_multiple = 0.5 * (
+            1.0 + math.cos(math.pi * num_cycles * 2.0 * progress)
+        )
+        return max(0.0, cosine_lr_multiple)
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
